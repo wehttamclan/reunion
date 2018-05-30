8d4525c9b56047ce0083709d3485aeb350f3206d
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-05-30 10:32:42 -0600

    adds activity class and tests.

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,29 @@
 class Activity
+  attr_reader :name, :base_cost, :cost_per_participant, :participants
+
+  def initialize(name, base_cost=0, cost_per_participant=0)
+    @name      = name
+    @base_cost = base_cost
+    @cost_per_participant = cost_per_participant
+    @participants     = Hash.new
+    # @who_owes_what = Hash.new
+  end
+
+  def add_participant(name, amount_paid)
+    @participants[name] = amount_paid
+  end
+
+  def total_cost
+    @base_cost + (@cost_per_participant*@participants.length).to_f
+  end
+
+  def fair_share
+    (total_cost/(@participants.length)).round(2)
+  end
+
+  def who_owes_what
+    @participants.merge(@participants) do |name, paid|
+      (fair_share - paid).round(2)
+    end
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,60 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def setup
+    @hiking = Activity.new('hiking')
+    @bowling = Activity.new('bowling', 10, 2)
+  end
+
+  def test_activity_has_a_name
+    assert_equal 'hiking', @hiking.name
+  end
+
+  def test_activity_has_associated_costs
+    assert_equal  0, @hiking.base_cost
+    assert_equal  0, @hiking.cost_per_participant
+    assert_equal 10, @bowling.base_cost
+    assert_equal  2, @bowling.cost_per_participant
+  end
+
+  def test_activity_has_no_participants_by_default
+    assert_equal 0, @hiking.participants.length
+    assert_equal 0, @bowling.participants.length
+  end
+
+  def test_activity_can_add_participant
+    @hiking.add_participant('Jake', 0)
+    assert_equal  1, @hiking.participants.length
+    @bowling.add_participant('Peter', 7)
+    @bowling.add_participant('Weber', 5)
+    assert_equal  2, @bowling.participants.length
+  end
+
+  def test_activity_has_total_costs
+    @hiking.add_participant('Jake', 0)
+    assert_equal  0, @hiking.total_cost
+    @bowling.add_participant('Peter', 7)
+    @bowling.add_participant('Weber', 5)
+    assert_equal 14, @bowling.total_cost
+  end
+
+  def test_activity_calculates_fair_share
+    @hiking.add_participant('Jake', 0)
+    assert_equal 0, @hiking.fair_share
+    @bowling.add_participant('Peter', 7)
+    @bowling.add_participant('Weber', 5)
+    @bowling.add_participant('Dude', 10)
+    assert_equal 5.33, @bowling.fair_share
+  end
+
+  def test_activity_calculates_what_particpants_owe
+    @hiking.add_participant('Jake', 0)
+    assert_equal 0, @hiking.fair_share
+    @bowling.add_participant('Peter', 7)
+    @bowling.add_participant('Weber', 5)
+    @bowling.add_participant('Dude', 10)
+    assert_equal (-1.67), @bowling.who_owes_what['Peter']
+    assert_equal  (0.33), @bowling.who_owes_what['Weber']
+    assert_equal (-4.67), @bowling.who_owes_what['Dude']
+  end
 end
