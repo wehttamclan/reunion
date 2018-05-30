d3dd8137d67a1842c56102109e8e3efe69355c84
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-05-30 10:33:05 -0600

    adds reunion class and tests.

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,2 +1,12 @@
 class Reunion
+  attr_reader :location, :activities
+
+  def initialize(location)
+    @location = location
+    @activities = []
+  end
+
+  def add_activity(activity)
+    @activities << activity
+  end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -4,4 +4,29 @@ require 'minitest/autorun'
 require './lib/reunion'
 
 class ReunionTest < Minitest::Test
+  def setup
+    @hiking = Activity.new('hiking')
+    @bowling = Activity.new('bowling', 10, 2)
+    @darts = Activity.new('darts', 5, 0)
+    @family_reunion = Reunion.new('Mexico')
+    @high_school_reunion = Reunion.new('Austin')
+  end
+
+  def test_reunion_has_location
+    assert_equal 'Mexico', @family_reunion.location
+    assert_equal 'Austin', @high_school_reunion.location
+  end
+
+  def test_reunion_has_no_activities_by_default
+    assert_equal [], @family_reunion.activities
+    assert_equal [], @high_school_reunion.activities
+  end
+
+  def test_reunion_can_add_activities
+    @family_reunion.add_activity(@hiking)
+    assert_equal [@hiking], @family_reunion.activities
+    @high_school_reunion.add_activity(@bowling)
+    @high_school_reunion.add_activity(@darts)
+    assert_equal [@bowling, @darts], @high_school_reunion.activities
+  end
 end
